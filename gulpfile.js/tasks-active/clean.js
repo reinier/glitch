433677f717e2ccc5d2931c2e9f4c51b560a66448
Reinier Ladan <reinierl@gmail.com>
2016-02-03 20:02:44 +0100

    Content update

--- gulpfile.js/tasks-active/clean.js
+++ gulpfile.js/tasks-active/clean.js
@@ -1,12 +1,8 @@
 var gulp      = require('gulp');
-var del       = require('del');
 var config    = require('../config').clean;
+var clean     = require('gulp-clean');
 
-/* Clean the public directory */
-gulp.task('clean', function (cb) {
-  del(config.all, cb);
-});
-
-gulp.task('clean:dist', function (cb) {
-  del(config.dist, cb);
+gulp.task('clean:dist', function () {
+  return gulp.src(config.dist, {read: false})
+    .pipe(clean());
 });
